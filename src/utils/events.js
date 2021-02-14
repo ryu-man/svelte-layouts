126ea365a696707aa85b742c92598319a702ec9f
ryu-man <rioman19@gmail.com>
2021-02-14 20:20:08 +0100

    Delete unecessairy files

--- src/utils/events.js
+++
@@ -1,4 +0,0 @@
-export function register(node, events) {
-    Object.keys(events).forEach((key) => node.addEventListener(key, events[key]));
-    return () => Object.keys(events).forEach((key) => node.removeEventListener(key, events[key]));
-}
