836e58c7ac8c0585df8deb598e7895d0c3528e0a
ryu-man <rioman19@gmail.com>
2021-02-19 01:09:11 +0100

    refactor: Improve css function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,7 @@
 export function css(node, style = {}) {
   const stylesheet = node.style
   Object.entries(style)
+    .filter((o) => o)
     .filter(([e1, e2]) => e2)
     .forEach((elem) => {
       stylesheet[elem[0]] = elem[1]
