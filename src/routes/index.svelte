e37fbe7fc1e2d2b27392d5e30b05b6e71daaf643
Abdelhalim Riache <rioman19@gmail.com>
2021-12-31 19:05:52 +0100

    Create an example

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,50 +2,58 @@
 	import { Row, Col, Grid, Item } from 'svelte-layouts';
 </script>
 
-<h1>svelet-layouts</h1>
+<Col width="100%" height="100%" gap="1em" justifyContent="center" alignItems="center">
+	<h1>svelet-layouts</h1>
+	<br>
+	<Grid
+		templateRows="auto"
+		templateCols="1fr 1fr 1fr"
+		colGap="4px"
+		style={{ width: '256px', height: '256px', backgroundColor: '#000', padding: '8px' }}
+	>
+		<Col gap="4px">
+			<div class="item" style="flex:1;" />
+			<div class="item" style="flex:1;" />
+			<div class="item" style="flex:1;" />
+			<div class="item" style="flex:1;" />
+			<div class="item" style="flex:1;" />
+			<Item class="item" grow="1" style={{ backgroundColor: '#bbbbbb' }} />
+		</Col>
+		<Grid
+			colStart="2"
+			colEnd="4"
+			templateCols="1fr 1fr"
+			templateRows="1fr 1fr 1fr"
+			colGap="4px"
+			rowGap="4px"
+		>
+			<div class="item" />
+			<div class="item" />
+			<div class="item" />
+			<div class="item" />
 
-<Grid
-	templateRows="auto"
-	templateCols="1fr 1fr 1fr"
-	style={{ width: '256px', height: '256px', backgroundColor: '#000' }}
->
-	<Col style={{ backgroundColor: '#adb5bd' }}>
-		<div class="item" style="flex:1;" />
-		<div class="item" style="flex:1;" />
-		<div class="item" style="flex:1;" />
-		<div class="item" style="flex:1;" />
-		<div class="item" style="flex:1;" />
-		<Item class="item" grow="1" />
-	</Col>
-	<Grid colStart="2" colEnd="4" templateCols="1fr 1fr" templateRows="1fr 1fr 1fr">
-		<div class="item" />
-		<div class="item" />
-		<div class="item" />
-		<div class="item" />
-
-		<Row colStart="1" colEnd="3" style={{ backgroundColor: '#adb5bd' }}>
-			<Grid class="flex row-item" templateCols="1fr 1fr 1fr 1fr">
-				<div class="item" style="flex:1;" />
-				<div class="item" style="flex:1;" />
-				<div class="item" style="flex:1;" />
-				<div class="item" style="flex:1;" />
-			</Grid>
-		</Row>
+			<Row colStart="1" colEnd="3" gap="4px">
+				<Item grow="1" style={{ backgroundColor: '#dddddd' }} />
+				<Item flex="1" style={{ backgroundColor: '#dddddd' }} />
+				<Item flex="1" style={{ backgroundColor: '#dddddd' }} />
+				<Item flex="1" style={{ backgroundColor: '#dddddd' }} />
+			</Row>
+		</Grid>
 	</Grid>
-</Grid>
+</Col>
 
-<style>
-	:global(.item) {
-		background-color: #adb5bd;
-		border: 2px solid #dee2e6;
+<style global>
+	html, body,
+	#svelte {
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		padding: 0;
 	}
-	:global(.flex) {
+	.flex {
 		flex: 1;
 	}
-	:global(.col-item) {
-		background-color: #ced4da;
-	}
-	:global(.row-item) {
-		background-color: #ced4da;
+	.item {
+		background-color: #bbbbbb;
 	}
 </style>
